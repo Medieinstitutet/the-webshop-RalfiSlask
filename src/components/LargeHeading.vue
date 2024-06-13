aafa76585ef2369067eeb517d834d4a7d28f2e9f
RalfiSlask <nilssonnarki@gmail.com>
2024-06-13 12:39:01 +0200

    ✨ add large heading component with text prop

---
+++ src/components/LargeHeading.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface ILargeHeading {
+  text: string;
+}
+
+defineProps<ILargeHeading>();
+</script>
+
+<template>
+  <h1 class="text-xl">{{ text }}</h1>
+</template>
