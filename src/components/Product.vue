12cc97abd99c0bc93d4e86e6dc1723bc449ab869
RalfiSlask <nilssonnarki@gmail.com>
2024-06-13 13:43:05 +0200

    ✨ add product component with productInfo props

---
+++ src/components/Product.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { IMovieProduct } from '@/utils/types/types';
+
+interface IProductProps {
+  productInfo: IMovieProduct;
+}
+
+defineProps<IProductProps>();
+</script>
+
+<template>
+  <div class="w-[125px] sm:w-[150px] md:w-[250px] xl:w-[300px] rounded-md py-4 flex flex-col gap-4">
+    <div class="px-2 flex flex-col gap-2 h-[125px]">
+      <h2 class="font-bold">{{ productInfo.name }}</h2>
+      <p>${{ productInfo.price }}</p>
+    </div>
+    <div class="w-full h-[200px] md:h-[300px]">
+      <img class="w-full h-full object-cover" :src="productInfo.imageUrl"></img>
+    </div>
+    <div class="px-2 overflow-y-auto h-[150px]">
+      <p>{{  productInfo.description }}</p>
+    </div>
+    
+  </div>
+
+</template>
