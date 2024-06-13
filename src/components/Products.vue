2fcdc1b085608bf7313e76171c02b2c76e20470e
RalfiSlask <nilssonnarki@gmail.com>
2024-06-13 13:43:51 +0200

    ✨ components component where i use the fetchShopData function and the products states for showing products

---
+++ src/components/Products.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+import Product from './Product.vue';
+import { onMounted } from 'vue';
+
+const { products, fetchShopData } = useWebshopStore();
+
+console.log(products);
+
+onMounted(() => {
+  fetchShopData('products');
+});
+</script>
+
+<template>
+  <section>
+    <ul class="flex flex-wrap gap-4 justify-center">
+      <Product v-for="product in products" :key="product.id" :productInfo="product" />
+    </ul>
+  </section>
+</template>
