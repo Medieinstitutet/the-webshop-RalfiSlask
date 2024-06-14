10056df3abfcf282816f2abf9a024cde09a71606
RalfiSlask <nilssonnarki@gmail.com>
2024-06-14 16:09:25 +0200

    ✨ add button for operation on button

---
+++ src/components/DecreaseProductButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+import { storeToRefs } from 'pinia';
+
+const store = useWebshopStore();
+const { decreaseCartProduct } = store;
+
+interface IButtonProps {
+  id: number;
+}
+
+defineProps<IButtonProps>();
+</script>
+
+<template>
+  <button @click="decreaseCartProduct(id)" class="operator-button">
+    <span class="mb-1">-</span>
+  </button>
+</template>
---
+++ src/components/IncreaseProductButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+import { storeToRefs } from 'pinia';
+
+const store = useWebshopStore();
+const { increaseCartProduct } = store;
+
+interface IButtonProps {
+  id: number;
+}
+
+defineProps<IButtonProps>();
+</script>
+
+<template>
+  <button @click="increaseCartProduct(id)" class="operator-button">
+    <span class="mb-1">+</span>
+  </button>
+</template>
