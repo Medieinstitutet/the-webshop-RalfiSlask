fbf17ba4bc218c555e2b3067b84442f8d203b3e9
RalfiSlask <nilssonnarki@gmail.com>
2024-06-14 10:56:42 +0200

    ✨ add products component with looped products and fetching products from api on mount

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,20 +2,28 @@
 import { useWebshopStore } from '@/stores/webshopStore';
 import Product from './Product.vue';
 import { onMounted } from 'vue';
+import SearchInput from './SearchInput.vue';
+import Categories from './Categories.vue';
+import { storeToRefs } from 'pinia';
 
-const { products, fetchShopData } = useWebshopStore();
-
-console.log(products);
+const store = useWebshopStore();
+const { filteredProducts } = storeToRefs(store);
+const { fetchProducts } = store;
 
 onMounted(() => {
-  fetchShopData('products');
+  fetchProducts();
 });
 </script>
 
 <template>
-  <section>
+  <section class="max-w-[1400px] flex flex-col items-center gap-10">
+    <h2 class="text-3xl">Products</h2>
+    <div class="w-full flex items-center justify-between">
+      <Categories />
+      <SearchInput />
+    </div>
     <ul class="flex flex-wrap gap-4 justify-center">
-      <Product v-for="product in products" :key="product.id" :productInfo="product" />
+      <Product v-for="product in filteredProducts" :key="product.id" :productInfo="product" />
     </ul>
   </section>
 </template>
