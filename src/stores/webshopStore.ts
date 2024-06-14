a46bc237bbdcd738fb97e6b69e4455ff08d3d779
RalfiSlask <nilssonnarki@gmail.com>
2024-06-14 16:06:04 +0200

    ✨ implement search API call and functionality for searching products. Also update filterering of products with a getter that keeps track of the states.

--- src/stores/webshopStore.ts
+++ src/stores/webshopStore.ts
@@ -7,23 +7,42 @@ interface IWebshopStates {
   products: IMovieProduct[];
   categories: ICategory[];
   selectedCategoryId: number;
-  filteredProducts: IMovieProduct[];
+  searchResults: number[];
   cartProducts: CartProduct[];
+  searchText: string;
 }
 
 export const useWebshopStore = defineStore('webshop', {
   state: (): IWebshopStates => ({
     products: [],
     categories: [],
-    selectedCategoryId: 1,
-    filteredProducts: [],
+    selectedCategoryId: 1, // All
     cartProducts: [{ title: 'hej', cartId: 1, price: 200, imgUrl: '', quantity: 1 }],
+    searchText: '',
+    searchResults: [],
   }),
   getters: {
     totalPrice: state => {
       return state.cartProducts.reduce((prevV, nextV) => {
         return prevV + nextV.price * nextV.quantity;
       }, 0);
+    },
+    visibleProducts: state => {
+      let products = state.products;
+      if (state.searchText.trim() === '') {
+        products = state.products;
+      } else {
+        products = state.products.filter(product => state.searchResults.includes(product.id));
+      }
+
+      if (state.selectedCategoryId !== 1) {
+        products = products.filter(product => {
+          return product.productCategory?.some(category => category.categoryId === state.selectedCategoryId);
+        });
+        console.log('products after filter:', products);
+      }
+
+      return products;
     },
   },
   actions: {
@@ -32,7 +51,7 @@ export const useWebshopStore = defineStore('webshop', {
         const response = await axios.get(`${import.meta.env.VITE_API_URL}/products`);
         const data = response.data;
         this.products = data;
-        this.filteredProducts = data;
+        console.log('products:', data);
       } catch (err) {
         console.log(err, 'error');
       }
@@ -47,15 +66,32 @@ export const useWebshopStore = defineStore('webshop', {
         console.log(err, 'error');
       }
     },
+    async fetchBySearch(searchText: string) {
+      if (searchText.trim() === '') {
+        searchText = '';
+        this.searchResults = [];
+        return;
+      }
+
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/search?searchtext=${searchText}`);
+        const data: IMovieProduct[] | {} = response.data;
+        if (Array.isArray(data)) {
+          // have to convert to array of numbers, productCategory is null in the API
+          this.searchResults = data.map(product => product.id);
+        } else {
+          this.searchResults = [];
+        }
+        this.searchText = searchText;
+      } catch (err) {
+        console.log(err, 'error');
+      }
+    },
     setSelectedCategory(categoryId: number): void {
       this.selectedCategoryId = categoryId;
-      if (categoryId === 1) {
-        this.filteredProducts = this.products;
-      } else {
-        this.filteredProducts = this.products.filter(product =>
-          product.productCategory.some(category => category.categoryId === this.selectedCategoryId)
-        );
-      }
+    },
+    setProductsBySearch(searchText: string) {
+      this.fetchBySearch(searchText);
     },
     addProductToCart(productId: number): void {
       const isProductAlreadyInCart = this.cartProducts.some(product => product.cartId === productId);
