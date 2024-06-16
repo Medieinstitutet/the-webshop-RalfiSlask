ed327af923517c1715c92459529ab4792903230f
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:13:15 +0200

    🚚 move to new location

--- src/components/Cart.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router';
-</script>
-
-<template>
-  <RouterLink to="/checkout">Cashier</RouterLink>
-</template>
---
+++ src/components/header/GoBackButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+
+const store = useWebshopStore();
+const { changeStateOfCartSidebar } = store;
+</script>
+
+<template>
+  <button @click="changeStateOfCartSidebar(false)">Go Back</button>
+</template>
