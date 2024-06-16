69747bfccc35084fd721ac91f0962e2b6be58e16
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:09:48 +0200

    ✨ add FooterInfo

---
+++ src/components/footer/FooterInfo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import FooterLinksList from './FooterLinksList.vue';
+import FooterList from './FooterList.vue';
+
+const lists = [
+  { id: 1, heading: 'Info', listItems: ['Shopping Policy', 'Return & Refund', 'Support', 'FAQs'] },
+  { id: 2, heading: 'Office', listItems: ['12345 Hej På Dig gatan', 'Stockholm', 'Sweden', '12-345-6789'] },
+];
+</script>
+
+<template>
+  <div class="flex gap-6">
+    <FooterLinksList />
+    <FooterList v-for="list in lists" :key="list.id" :listInfo="list" />
+  </div>
+</template>
