ba008340ec2e40e6b6b147ae3ff0275424f3e0e7
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:14:20 +0200

    ✨ add payment component

---
+++ src/components/checkout/PaymentMethods.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Payment from './Payment.vue';
+</script>
+
+<template>
+  <div class="flex justify-between w-full">
+    <h2>Payment Methods</h2>
+    <div class="flex flex-col items-end gap-4 w-[50%]">
+      <Payment paymentType="invoice" />
+      <Payment paymentType="paypal" />
+    </div>
+  </div>
+</template>
