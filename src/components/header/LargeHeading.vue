6b341d329f9b466acf73bf4a933fbb0c6ab510f3
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:11:27 +0200

    ✨ Large Heading reusable component

---
+++ src/components/header/LargeHeading.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface ILargeHeadingProps {
+  text: string;
+}
+
+defineProps<ILargeHeadingProps>();
+</script>
+
+<template>
+  <h1 class="text-xl">{{ text }}</h1>
+</template>
