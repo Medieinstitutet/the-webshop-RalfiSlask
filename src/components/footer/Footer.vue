f2e442a832b83a98f624d2e5dbf0915201b62b96
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:13:28 +0200

    ✨ add footer component

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FooterInfo from './FooterInfo.vue';
+</script>
+
+<template>
+  <footer class="w-full flex flex-col items-center pt-12 pb-8 px-[5%] bg-blackColor text-white absolute bottom-0">
+    <div class="flex items-center justify-between w-full">
+      <div>Matthias Webshop</div>
+      <FooterInfo />
+    </div>
+    <div class="border-t border-solid border-white w-full">
+      <div class="flex gap-4 items-center text-sm py-6">
+        <p>Copyright @ 2024 Matthias</p>
+        <p>|</p>
+        <p class="text-gray-500">Privacy Policy Terms & Conditions</p>
+      </div>
+    </div>
+  </footer>
+</template>
