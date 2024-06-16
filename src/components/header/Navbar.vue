d61a757ce7dcac58c2f1bb20671abdcf41146095
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:12:35 +0200

    ✨add navbar with routerlinks

---
+++ src/components/header/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="flex items-center gap-4 text-lg">
+    <nav class="flex items-center gap-2">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/products">Products</RouterLink>
+      <RouterLink to="/admin">Admin</RouterLink>
+    </nav>
+  </div>
+</template>
