6e580426c635687f7f4cd54bb7cecda30f8dd1e1
RalfiSlask <nilssonnarki@gmail.com>
2024-06-16 18:08:11 +0200

    💄 🔧 add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenColor: '#38CB89',
+        grayColor: '#F3F5F7',
+        blackColor: '#141718',
+        grayColor: '#807E7E',
+        greenHover: '#5FE3A9',
+      },
+    },
   },
   plugins: [],
 };
