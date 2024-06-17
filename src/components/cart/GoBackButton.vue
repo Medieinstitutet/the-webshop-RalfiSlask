f981572620abcecdf8e8584aa2fb3151046957e7
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 19:10:50 +0200

    ✨ add go back button

---
+++ src/components/cart/GoBackButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+
+const store = useWebshopStore();
+const { changeStateOfModal } = store;
+</script>
+
+<template>
+  <button @click="changeStateOfModal('cart', false)">Go Back</button>
+</template>
