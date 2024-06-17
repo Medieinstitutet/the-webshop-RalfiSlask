0d5e642c34e8997472e12ce9bf851349357d467e
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 19:12:34 +0200

    ✨ add form for the checkout

---
+++ src/components/checkout/CartForm.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FormSection from './FormSection.vue';
+import { formInputs } from '@/models/FormInputs';
+</script>
+
+<template>
+  <form class="w-full">
+    <div class="flex flex-col gap-4">
+      <FormSection v-for="form in formInputs" :key="form.id" :formInfo="form" />
+    </div>
+  </form>
+</template>
