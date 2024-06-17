c26cfb9df1ee7ce40f76ef846c9a4d6abb1b62c2
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 19:09:29 +0200

    ✨ add checkout button

---
+++ src/components/cart/CheckoutButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { useWebshopStore } from '@/stores/webshopStore';
+
+const store = useWebshopStore();
+
+const { changeStateOfModal } = store;
+</script>
+
+<template>
+  <RouterLink to="/checkout">
+    <button @click="changeStateOfModal('cart', false)" class="primary-button">Checkout</button>
+  </RouterLink>
+</template>
