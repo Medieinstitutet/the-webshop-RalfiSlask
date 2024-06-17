8646dad9360b208ec12ab38907a4fdf2f3079fab
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 19:10:24 +0200

    ✨ add pay button

---
+++ src/components/checkout/PayButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+import { storeToRefs } from 'pinia';
+const store = useWebshopStore();
+
+const { postOrders } = store;
+
+const { isCheckoutFormValid, checkutFormError } = storeToRefs(store);
+
+const handleClickOnPayButton = () => {
+  if (isCheckoutFormValid.value) {
+    postOrders();
+    console.log('valid');
+  } else {
+    checkutFormError.value = true;
+  }
+};
+</script>
+
+<template>
+  <button @click="handleClickOnPayButton" class="primary-button">Pay</button>
+</template>
