11cd083a3eb730848d955982c1010c85ae17c5bb
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 15:36:09 +0200

    ✨add operation button component

---
+++ src/components/cart/RemoveCartProductButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface IRemoveButtonProps {
+  productId: number;
+}
+
+defineProps<IRemoveButtonProps>();
+
+defineEmits<{
+  (e: 'remove-product', productId: number): void;
+}>();
+</script>
+
+<template>
+  <button @click="$emit('remove-product', productId)">Remove</button>
+</template>
