ea7c34e82c051868d7f1e18ffb9c39a81190dc26
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 15:35:26 +0200

    ✨ add reusable redirect button with props

---
+++ src/components/shared/RedirectButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { useWebshopStore } from '@/stores/webshopStore';
+
+const store = useWebshopStore();
+const { resetCheckoutForm } = store;
+
+interface IRedirectButtonProps {
+  path: string;
+  title: string;
+}
+
+defineProps<IRedirectButtonProps>();
+</script>
+
+<template>
+  <RouterLink to="/products"
+    ><button @click="resetCheckoutForm">{{ title }}</button></RouterLink
+  >
+</template>
