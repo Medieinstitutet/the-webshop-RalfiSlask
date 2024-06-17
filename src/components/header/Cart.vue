dfbc3037e5d5c9bee0e2d8b3b4d11d4ef2264517
RalfiSlask <nilssonnarki@gmail.com>
2024-06-17 11:05:13 +0200

    ✨ add cart component with functionality for opening sidebar

---
+++ src/components/header/Cart.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+import { storeToRefs } from 'pinia';
+
+const store = useWebshopStore();
+const { changeStateOfModal } = store;
+const { cartProducts } = storeToRefs(store);
+</script>
+
+<template>
+  <button @click="changeStateOfModal('cart', true)" class="relative">
+    <span>To Cashier</span>
+    <span class="flex items-center gap-2 bg-grayColor w-6 justify-center rounded-full absolute right-0 top-[-20px]">
+      <p>{{ cartProducts.length }}</p>
+    </span>
+  </button>
+</template>
