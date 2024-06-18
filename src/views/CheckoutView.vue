e9342c27c76c972773c3edd2f97b81fbf4cdc208
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:51:05 +0200

    ✨ import components and styling

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,7 +1,27 @@
+<script setup lang="ts">
+import CartForm from '@/components/checkout/CartForm.vue';
+import PaymentMethods from '@/components/checkout/PaymentMethods.vue';
+import PayButton from '@/components/checkout/PayButton.vue';
+import LargeHeading from '@/components/shared/LargeHeading.vue';
+import ThankYouForOrder from '@/components/checkout/ThankYouForOrder.vue';
+import { useWebshopStore } from '@/stores/webshopStore';
+import { storeToRefs } from 'pinia';
+
+const store = useWebshopStore();
+
+const { isOrderPayed } = storeToRefs(store);
+</script>
+
 <template>
-  <div></div>
+  <div class="w-full max-w-[350px] sm:max-w-[450px] md:max-w-[800px] mb-[500px]">
+    <ThankYouForOrder v-if="isOrderPayed" />
+    <div v-else class="px-2 md:px-10 py-12 flex flex-col items-center md:items-start gap-10">
+      <LargeHeading text="Checkout" />
+      <div class="flex flex-col gap-10">
+        <CartForm />
+        <PaymentMethods />
+        <PayButton />
+      </div>
+    </div>
+  </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
