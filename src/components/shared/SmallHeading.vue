13b060ffbf80294985456ca314e0e93139a835ef
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:39:33 +0200

    ✨ add reusable heading

---
+++ src/components/shared/SmallHeading.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { getTextWithFirstLetterUpperCase } from '@/utils/helperfunctions/helperfunctions';
+interface ILargeHeadingProps {
+  text: string;
+}
+
+defineProps<ILargeHeadingProps>();
+</script>
+
+<template>
+  <h2 class="text-lg font-medium">{{ getTextWithFirstLetterUpperCase(text) }}</h2>
+</template>
