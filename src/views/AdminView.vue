0da86413a075018ce9838edb99f0bf7d6b949e59
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:50:36 +0200

    ✨ add components and styling

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+import { useWebshopStore } from '@/stores/webshopStore';
+import AdminSection from '@/components/admin/AdminSection.vue';
+import { onMounted } from 'vue';
+import LargeHeading from '@/components/shared/LargeHeading.vue';
+import { storeToRefs } from 'pinia';
+
+const store = useWebshopStore();
+
+const { companyId } = storeToRefs(store);
+const { fetchOrdersByCompany } = store;
+
+onMounted(() => {
+  fetchOrdersByCompany(companyId.value);
+});
+</script>
+
 <template>
-  <div></div>
+  <section
+    class="flex flex-col items-start w-full mt-10 max-w-[350px] sm:max-w-[500px] md:max-w-[800px] xl:max-w-[1100px]"
+  >
+    <LargeHeading text="Orders" />
+    <AdminSection />
+  </section>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
