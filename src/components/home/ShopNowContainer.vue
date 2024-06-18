368fc5584cb0759424c5edc059c2abbf678e1673
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:46:21 +0200

    ✨ add shop now container

---
+++ src/components/home/ShopNowContainer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div
+    class="flex flex-col gap-4 px-6 py-6 bg-secondaryColor rounded-lg absolute right-1/2 md:right-[25%] md:bottom-[15%] translate-x-1/2"
+  >
+    <div class="w-[300px] flex flex-col gap-4">
+      <h2 class="text-black uppercase text-2xl xl:text-3xl font-medium">Cinema Magic, Delivered to you.</h2>
+      <p class="text-blackColor">
+        "Discover and purchase your favorite movies effortlessly from our curated collection."
+      </p>
+    </div>
+    <RouterLink to="/products">
+      <button class="primary-button">Shopping</button>
+    </RouterLink>
+  </div>
+</template>
