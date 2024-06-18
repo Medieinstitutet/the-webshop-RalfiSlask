45369cd91cc02c16bf89949064c5c16ed5a54de2
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:48:43 +0200

    ✨ import and use the product slider and fix positioning and structure of the product container

--- src/components/products/Product.vue
+++ src/components/products/Product.vue
@@ -3,10 +3,11 @@ import type { IMovieProduct } from '@/utils/types/types';
 import { useWebshopStore } from '@/stores/webshopStore';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
+import ProductInfoSlider from './ProductInfoSlider.vue';
 
 const store = useWebshopStore();
 
-const { cartProducts } = storeToRefs(store)
+const { cartProducts } = storeToRefs(store);
 
 const { addProductToCart } = store;
 
@@ -14,30 +15,42 @@ interface IProductProps {
   productInfo: IMovieProduct;
 }
 
-const props = defineProps<IProductProps>()
+const props = defineProps<IProductProps>();
 
 const isProductInCart = computed(() => {
-  return cartProducts.value.some(product => product.cartId === props.productInfo.id);
-})
+  return cartProducts.value.some(product => product.productId === props.productInfo.id);
+});
 
-
+const { name, price, imageUrl, description, id, year } = props.productInfo;
 </script>
 
 <template>
   <div class="product">
     <div class="px-2 flex flex-col gap-2 h-[125px] w-full">
-      <h2 class="font-bold">{{ productInfo.name }}</h2>
-      <p>${{ productInfo.price }}</p>
+      <h2 class="font-bold">{{ name }}</h2>
+      <p>${{ price }}</p>
     </div>
-    <div class="w-full h-[200px] md:h-[300px]">
-      <img class="w-full h-full object-cover" loading="lazy" :src="productInfo.imageUrl" :alt="productInfo.description" width="200px" height="300px"></img>
+    <div class="overflow-y-hidden relative w-full h-[450px] md:h-[550px]">
+      <div class="w-full h-full absolute top-0 z-10">
+        <img
+          class="w-full h-full object-cover"
+          loading="lazy"
+          :src="imageUrl"
+          :alt="description"
+          width="200px"
+          height="300px"
+        />
+      </div>
+      <div class="group absolute bottom-0 z-30 w-full">
+        <ProductInfoSlider :year="year.toString()" :description="description" />
+        <button
+          class="absolute bottom-0 secondary-button z-40"
+          :disabled="isProductInCart"
+          @click="addProductToCart(id)"
+        >
+          {{ isProductInCart ? 'In Cart' : 'Buy' }}
+        </button>
+      </div>
     </div>
-    <div class="w-full">
-      <p>Year: {{ productInfo.year }}</p>
-    </div>
-    <div class="px-2 overflow-y-auto h-[150px]">
-      <p>{{  productInfo.description }}</p>
-    </div>
-    <button class="primary-button" :disabled="isProductInCart" @click="addProductToCart(productInfo.id)">{{ isProductInCart ? 'In Cart' : 'Buy'}}</button>
   </div>
 </template>
