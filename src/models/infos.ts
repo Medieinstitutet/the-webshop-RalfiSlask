d6d3921e87283396809cb8d6f9ab4a80b6cdac2e
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:40:25 +0200

    ✨ add infos array for the homepage

---
+++ src/models/infos.ts
@@ -0,0 +1,28 @@
+import type { IPositiveInfo } from '@/utils/types/types';
+
+export const infos: IPositiveInfo[] = [
+  {
+    id: 1,
+    title: 'Free Delivery',
+    description: 'Delivered to your doorstep!',
+    imageUrl: '/delivery.svg',
+    alt: 'delivery icon',
+    iconSize: 'medium',
+  },
+  {
+    id: 2,
+    title: 'Support 24/7',
+    description: 'Use our amazing support',
+    imageUrl: '/customer.svg',
+    alt: 'customer service icon',
+    iconSize: 'small',
+  },
+  {
+    id: 3,
+    title: '100% Authentic',
+    description: 'Pride ourself in authenticity!',
+    imageUrl: '/certified.svg',
+    alt: 'authenticity icon',
+    iconSize: 'small',
+  },
+];
