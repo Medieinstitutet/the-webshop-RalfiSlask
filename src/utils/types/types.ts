4e4933d324f4f0ff664df06f504cae4dc25dffc3
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:40:50 +0200

    🏷️ add types for orders, info and inputs

--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -29,3 +29,41 @@ export interface IPaymentMethods {
   invoice: boolean;
   paypal: boolean;
 }
+
+export interface IInputs {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  city: string;
+  zip: string;
+  country: string;
+}
+
+export interface IOrder {
+  companyId: number;
+  created: Date;
+  createdBy: string;
+  id: number;
+  orderRows: IOrderProduct[];
+  paymentMethod: string;
+  status: number;
+  totalPrice: number;
+}
+
+export interface IOrderProduct {
+  id: number;
+  productId: number;
+  product: null;
+  amount: number;
+  orderId: number;
+}
+
+export interface IPositiveInfo {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  alt: string;
+  iconSize: 'small' | 'medium';
+}
