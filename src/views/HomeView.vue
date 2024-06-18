0135ef4e7a2e3dcf7a05f0d1e2f04fd59b7bc23b
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:49:48 +0200

    ✨ improve the homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ShopNowContainer from '../components/home/ShopNowContainer.vue';
+import { infos } from '@/models/infos';
+import PositiveInfo from '@/components/home/PositiveInfo.vue';
+</script>
 
 <template>
-  <main></main>
+  <section class="w-full pb-[300px] flex-flex-col gap-20">
+    <div class="relative w-full h-[500px] xl:h-[600px] bg-gray-200 flex items-center">
+      <div class="absolute -translate-x-1/2 left-1/2">
+        <img src="../assets/images/movies.png" alt="star wars movies" width="700" height="500" />
+      </div>
+      <ShopNowContainer />
+    </div>
+    <div class="w-full h-[250px] md:h-[150px] flex justify-center px-4 md:px-10 bg-secondaryColor text-black">
+      <div class="flex items-center justify-between w-full max-w-[1400px]">
+        <PositiveInfo v-for="info in infos" :key="info.id" :info="info" />
+      </div>
+    </div>
+  </section>
 </template>
