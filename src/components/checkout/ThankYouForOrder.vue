be1461727e16eab824275d10a8aef8a13de3d27d
RalfiSlask <nilssonnarki@gmail.com>
2024-06-18 18:44:09 +0200

    ✨ Thank you for your order component

---
+++ src/components/checkout/ThankYouForOrder.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LargeHeading from '../shared/LargeHeading.vue';
+import { RouterLink } from 'vue-router';
+import { useWebshopStore } from '@/stores/webshopStore';
+
+const store = useWebshopStore();
+const { toggleStateOfOrder } = store;
+</script>
+
+<template>
+  <div class="w-[500px] h-[500px] z-50">
+    <LargeHeading text="Thank you for your order!" />
+    <RouterLink to="/products">
+      <button @click="toggleStateOfOrder" class="primary-button">Back To Products</button>
+    </RouterLink>
+  </div>
+</template>
