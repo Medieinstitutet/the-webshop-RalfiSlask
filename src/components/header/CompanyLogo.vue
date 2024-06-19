b51312edfc791740df92b8e131ba71472261fdcc
RalfiSlask <nilssonnarki@gmail.com>
2024-06-19 12:09:36 +0200

    ✨ add company logo

---
+++ src/components/header/CompanyLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center gap-4 w-[170px]">
+    <img src="../../assets/images/movie.png" alt="company logo" width="40" height="40" />
+    <h1 class="text-xl font-medium uppercase">Webshop</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
