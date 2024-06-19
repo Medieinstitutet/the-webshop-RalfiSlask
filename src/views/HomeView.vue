5d5b892be75021925374efb31346385b4570dd69
RalfiSlask <nilssonnarki@gmail.com>
2024-06-19 12:11:36 +0200

    ♻️ Refactor by using components instead to simplify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
-import ShopNowContainer from '../components/home/ShopNowContainer.vue';
-import { infos } from '@/models/infos';
-import PositiveInfo from '@/components/home/PositiveInfo.vue';
+import MailBag from '../components/home/MailBag.vue';
+import UpperSection from '../components/home/UpperSection.vue';
+import TarantinoSection from '@/components/home/TarantinoSection.vue';
 </script>
 
 <template>
-  <section class="w-full pb-[300px] flex-flex-col gap-20">
-    <div class="relative w-full h-[500px] xl:h-[600px] bg-gray-200 flex items-center">
-      <div class="absolute -translate-x-1/2 left-1/2">
-        <img src="../assets/images/movies.png" alt="star wars movies" width="700" height="500" />
-      </div>
-      <ShopNowContainer />
-    </div>
-    <div class="w-full h-[250px] md:h-[150px] flex justify-center px-4 md:px-10 bg-secondaryColor text-black">
-      <div class="flex items-center justify-between w-full max-w-[1400px]">
-        <PositiveInfo v-for="info in infos" :key="info.id" :info="info" />
-      </div>
-    </div>
+  <section class="w-full flex flex-col gap-40 items-center">
+    <UpperSection />
+    <TarantinoSection />
+    <MailBag />
   </section>
 </template>
